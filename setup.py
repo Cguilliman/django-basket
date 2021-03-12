d0d6792b14164648e8b96f628853746c3d3bca2d
CGuilliman <cguilliman@gmail.com>
2021-03-12 16:49:24 +0200

    Change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     pkj_name = 'django_basket'
 
     setup(
-        name='django-basket',
+        name='django-ib-basket',
         version='0.1',
         long_description_content_type='text/x-rst',
         packages=[pkj_name] + [pkj_name + '.' + x for x in find_packages(pkj_name)],
